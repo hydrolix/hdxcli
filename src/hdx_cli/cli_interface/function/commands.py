df49ffb4bd80d1b3e5e02373c5ca2d6beb6c319f
Germán Diago <german@hydrolix.io>
2022-12-06 12:13:57 +0700

    Implemented specific-function create

--- src/hdx_cli/cli_interface/function/commands.py
+++ src/hdx_cli/cli_interface/function/commands.py
@@ -10,8 +10,7 @@ from ...library_api.common import rest_operations as rest_ops
 from ...library_api.utility.decorators import report_error_and_exit
 from ...library_api.common.exceptions import HdxCliException, LogicException
 
-from ..common.rest_operations import (create as command_create,
-                                      delete as command_delete,
+from ..common.rest_operations import (delete as command_delete,
                                       list_ as command_list,
                                       show as command_show)
 
@@ -39,7 +38,51 @@ def function(ctx: click.Context):
                'usercontext': profileinfo}
 
 
-function.add_command(command_create)
+@click.command(help='Create sql function.')
+@click.option('--sql-from-file', '-f',
+              help='Create the body of the sql from a json description as in the POST request in https://docs.hydrolix.io/docs/custom-functions.'
+              """For example:
+              '{
+                "sql": "(x, k, b) -> k*x + b;",
+                "name": "linear_equation"
+              }'"""
+              ". 'name' will be replaced by FUNCTION_NAME",
+              default=None)
+@click.option('--inline-sql', '-s',
+              help="Use inline sql in the command-line",
+              default=None)
+@click.argument('function_name')
+@click.pass_context
+@report_error_and_exit(exctype=HdxCliException)
+def create(ctx: click.Context,
+           function_name: str,
+           sql_from_file,
+           inline_sql):
+    if inline_sql and sql_from_file:
+        raise LogicException('Only one of the options --inline-sql and --sql-from-file can be used.')
+    if not inline_sql and not sql_from_file:
+        raise LogicException('You need at least one of --inline-sql or --sql-from-file to create a function.')
+
+    resource_path = ctx.parent.obj['resource_path']
+    profile = ctx.parent.obj['usercontext']
+    hostname = profile.hostname
+    url = f'https://{hostname}{resource_path}'
+    token = profile.auth
+    headers = {'Authorization': f'{token.token_type} {token.token}',
+               'Accept': 'application/json'}
+    body = {}
+    if sql_from_file:
+        with open(sql_from_file, 'r', encoding='utf-8') as input_body:
+            body = json.load(input_body)
+            body['name'] = f'{function_name}'
+    elif inline_sql:
+        body['name'] = f'{function_name}'
+        body['sql'] = inline_sql
+    rest_ops.create(url, body=body, headers=headers)
+    print(f'Created function {function_name}.')
+
+
+function.add_command(create)
 function.add_command(command_delete)
 function.add_command(command_list)
 function.add_command(command_show)
