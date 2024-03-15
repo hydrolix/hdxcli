19813fd9911d9e430588a0fb1c10917edf1ec82f
Agustin Actis <103114098+agustinhydrolix@users.noreply.github.com>
2024-03-15 12:33:19 -0300

    refactored create table command due to changes in summary tables (#28)

--- src/hdx_cli/main.py
+++ src/hdx_cli/main.py
@@ -32,7 +32,7 @@ from hdx_cli.library_api.common.profile import save_profile, get_profile_data_fr
 from hdx_cli.library_api.common.auth_utils import load_user_context
 from hdx_cli.library_api.common.logging import set_debug_logger, set_info_logger, get_logger
 
-VERSION = "1.0-rc48"
+VERSION = "1.0-rc49"
 
 
 from hdx_cli.library_api.common.auth import (
