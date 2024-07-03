568f838d877794575b220480291f4062e16b95f0
luzsantiago <luz@hydrolix.io>
2024-07-03 14:58:12 -0300

    change output batch

--- tests/command_line_interface/test_cases/test_read_only_flows.ts
+++ tests/command_line_interface/test_cases/test_read_only_flows.ts
@@ -421,7 +421,7 @@ expected_output = 'Updated test_ci_storage settings.cloud'
 [[test]]
 name = "Storages can be shown"
 commands_under_test = ["python3 -m hdx_cli.main storage --storage test_ci_storage show"]
-expected_output_re = '.*? "is_default": false, "bucket_name": "qe-multi-bucket-2", "bucket_path": "/cli/", "region": "us-central1", "cloud": "gcp", "credential_id": null .*'
+expected_output_expr = 'not result.startswith("Error:") and "name" in result and "uuid" in result and "settings" in result and "bucket_name" in result and "test_ci_storage" in result'
 
 
 ####################################################### Batch Job ######################################################
@@ -454,12 +454,11 @@ expected_output = 'Retrying test_ci_batch_job'
 [[test]]
 name = "Batch job settings can be shown"
 commands_under_test = ["python3 -m hdx_cli.main job batch --job test_ci_batch_job settings"]
-expected_output_expr = 'not result.startswith("Error:") and "name" in result and "type" in result and "value" in result and "test_ci_batch_job" in result'
-
+expected_output_re = '.*?name                                                                                      string                        "test_ci_batch_job".*'
 [[test]]
 name = "Batch jobs can be shown"
 commands_under_test = ["python3 -m hdx_cli.main job batch --job test_ci_batch_job show"]
-expected_output_expr = 'not result.startswith("Error:") and "name" in result and "uuid" in result and "settings" in result and "status" in result and "test_ci_batch_job" in result'
+expected_output_re = '.*?"name": "test_batch_job", "description": null.*'
 
 [[test]]
 name = "Batch jobs can be deleted"
