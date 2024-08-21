e9bb478b809fbf334230fd79b9730ba441503ce4
Agustin Actis <103114098+agustinhydrolix@users.noreply.github.com>
2024-08-21 14:14:46 -0300

    Removed summary command (#42)
    * summary command was removed from sources

--- src/hdx_cli/cli_interface/sources/commands.py
+++ src/hdx_cli/cli_interface/sources/commands.py
@@ -1,9 +1,9 @@
 """Commands relative to sources handling  operations"""
+
 import click
 
 from .kafka import kafka as command_kafka
 from .kinesis import kinesis as command_kinesis
-from .summary import summary as command_summary
 from .siem import siem as command_siem
 
 
@@ -14,5 +14,4 @@ def sources():
 
 sources.add_command(command_kafka)
 sources.add_command(command_kinesis)
-sources.add_command(command_summary)
 sources.add_command(command_siem)
