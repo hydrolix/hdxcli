950718d417c3f3d96ab5fcfa2df82886a1c98495
Scott Rubin <scott@hydrolix.io>
2025-03-12 15:16:15 -0400

    Allow setting hosts with port numbers in profile

--- src/hdx_cli/library_api/common/validation.py
+++ src/hdx_cli/library_api/common/validation.py
@@ -15,5 +15,6 @@ def is_valid_hostname(hostname):
         return False
     if hostname[-1] == ".":
         hostname = hostname[:-1]  # strip exactly one dot from the right, if present
-    allowed = re.compile(r"(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    pattern = r"^([\w\d][\w\d\.\-]+[\w\d])(\:([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]))?$"
+    allowed = re.compile(pattern, re.IGNORECASE)
     return all(allowed.match(x) for x in hostname.split("."))
