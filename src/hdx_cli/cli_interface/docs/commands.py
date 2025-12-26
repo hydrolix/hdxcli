2018f65b898167a34c4a401eefed452e3733680b
Martin A. Brown <mabrown@hydrolix.io>
2025-12-26 13:25:32 -0800

    docs: decrease blank line count, avoid block quote (Markdown)
    The Markdown parser (`pymdown`) in use by the documentation team expects
    only a single blank line between paragraphs [MD032].
    
    Switch from using a block quotation with only a single emphasized string
    to display version number to a full line of text [MD036].
    
    This is a small code change that allows the generated Markdown to
    satisfy the `pymarkdown` rules used by the Hydrolix docs system.
    
    - https://pymarkdown.readthedocs.io/en/latest/plugins/rule_md032/
    - https://pymarkdown.readthedocs.io/en/latest/plugins/rule_md036/

--- src/hdx_cli/cli_interface/docs/commands.py
+++ src/hdx_cli/cli_interface/docs/commands.py
@@ -75,7 +75,7 @@ def _generate_full_docs(root_ctx: click.Context, output_path: Path):
         frontmatter_parts.append("---")
 
         frontmatter = "\n".join(frontmatter_parts)
-        version_line = f"> _hdxcli v{VERSION}_"
+        version_line = f"Documentation covers _hdxcli v{VERSION}_"
 
         # The markdown file starts with the frontmatter and the version line
         final_md_content = f"{frontmatter}\n\n{version_line}\n\n"
@@ -88,7 +88,7 @@ def _generate_full_docs(root_ctx: click.Context, output_path: Path):
                 md_content = command.to_markdown(cmd_ctx)
                 body_parts.append(md_content)
 
-        final_md_content += "\n\n---\n\n".join(body_parts)
+        final_md_content += "\n---\n\n".join(body_parts)
 
         file_path = output_path / group_filename
         file_path.write_text(final_md_content, encoding="utf-8")
